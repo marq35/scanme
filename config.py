a28b7c46337e84c16e7a66b0b7d1ed5c12d3d997
Marcin <marq35@op.pl>
2016-07-25 19:45:39 +0200

    Fixes in mail configuration.

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[ScanMe]'
-    FLASKY_MAIL_SENDER = 'ScanMe Admin <marq35@gmail.com.com>'
+    FLASKY_MAIL_SENDER = 'ScanMe Admin <marq35@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
