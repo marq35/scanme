ea796f86254d72734bd7e10c46c42b671a120db4
Marcin <marq35@op.pl>
2016-07-26 22:05:04 +0200

    Libraries added to app init.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask.ext.mail import Mail
 from flask_moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
 
 bootstrap = Bootstrap()
+mail = Mail
 moment = Moment()
+db = SQLAlchemy()
 
 
 def create_app(config_name):
