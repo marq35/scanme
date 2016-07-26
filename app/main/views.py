e95330fa50c92cb3967e34c986163906de66d15f
Marcin <marq35@op.pl>
2016-07-26 22:08:55 +0200

    error handling updates

--- app/main/views.py
+++ app/main/views.py
@@ -4,11 +4,3 @@ from . import main
 @main.route('/')
 def index():
     return render_template('index.html')
-
-@main.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@main.errorhandler(500)
-def page_not_found(e):
-    return render_template('500.html'), 500
