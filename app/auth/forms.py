7a7f5a1e2774fd47bae0c3d8b477ebd64329b7a2
marq <nobile>
2016-08-08 15:17:49 +0200

    Added login/logout templates, form and views.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import Required, Email
 
 
 class LoginForm(Form):
-    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    email = StringField('Email', validators=[Required(), Email()])
     password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log in')
+    submit = SubmitField('Log In')
