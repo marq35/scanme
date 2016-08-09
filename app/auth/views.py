ced8446e6461d6880ef0f23a71368f723e3c9095
Marcin <marq35@op.pl>
2016-08-09 21:43:13 +0200

    added missed form send methods

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,7 +5,7 @@ from ..models import User
 from .forms import LoginForm
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -13,7 +13,7 @@ def login():
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid username or password')
+        flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
 
 
