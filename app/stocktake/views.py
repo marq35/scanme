a9f8169cee47d4fc186dacdaaa3b134d8782a67e
Marcin <marq35@op.pl>
2016-09-07 21:17:45 +0200

    Fixes in names for items config variables. Added variable for paginate of stocktakes.

--- app/stocktake/views.py
+++ app/stocktake/views.py
@@ -1,9 +1,24 @@
-from flask import render_template
+from flask import render_template, abort, flash, redirect, url_for, request,\
+    current_app
 from flask.ext.login import login_required
 from . import stocktake
+from ..models import User, Stocktake
 
 
 @login_required
-@stocktake.route('/menu')
+@stocktake.route('/menu', methods=['GET', 'POST'])
 def menu():
-    return render_template('stocktake/menu.html')
+    page = request.args.get('page', 1, type=int)
+    st_count = Stocktake.query.count()
+    pagination = Stocktake.query.order_by(Stocktake.start_date).paginate(
+        page, per_page=current_app.config['SCANME_STOCKS_PER_PAGE'],
+        error_out=False)
+    stocks = pagination.items
+    return render_template('stocktake/menu.html', stocks=stocks,
+                           pagination=pagination, st_count=st_count)
+
+
+@login_required
+@stocktake.route('/delete')
+def delete():
+    pass
